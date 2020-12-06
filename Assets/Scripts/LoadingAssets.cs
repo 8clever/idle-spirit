685a7b028855019485b5129ce023472b9f060107
Ivan <godofluck89@gmail.com>
2020-12-06 22:48:04 +0300

    initial commit

---
+++ Assets/Scripts/LoadingAssets.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoadingAssets : MonoBehaviour {
+    public AssetReference scene;
+
+    private void Start() {
+        Addressables.LoadSceneAsync(scene);
+    }
+}
